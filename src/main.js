5dd116bdb576330a77867e4895d25dc1fa09f0a9
hejunjie04 <hejunjie04@baidu.com>
2023-01-25 16:15:39 +0800

    搭建cli项目模板

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import smartyUi from '@hejunjietest/smarty-ui-vite';
+import '@hejunjietest/smarty-ui-vite/dist/style.css';
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(smartyUi)
+    .mount('#app')
